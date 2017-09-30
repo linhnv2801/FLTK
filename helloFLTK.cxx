81d0f3a42cbd882dbc02f3078c407ae6547413c5
linhnv2801 <mrlinhnv2801@gmail.com>
2017-09-30 09:39:47 +0700

    change button

--- helloFLTK.cxx
+++ helloFLTK.cxx
@@ -1,13 +1,19 @@
 #include <FL/Fl.H>
 #include <FL/Fl_Window.H>
 #include <FL/Fl_Box.H>
+#include <Fl/Fl_Button.H>
 int main(int argc, char **argv) {
   Fl_Window *window = new Fl_Window(340,180);
-  Fl_Box *box = new Fl_Box(20,40,300,100,"Hello, World!");
-  box->box(FL_UP_BOX);
-  box->labelfont(FL_BOLD+FL_ITALIC);
-  box->labelsize(36);
-  box->labeltype(FL_SHADOW_LABEL);
+
+  // Fl_Box *box = new Fl_Box(20,40,300,100,"Hello, World!");
+  // box->box(FL_UP_BOX);
+  // box->labelfont(FL_BOLD+FL_ITALIC);
+  // box->labelsize(36);
+  // box->labeltype(FL_SHADOW_LABEL);
+
+  Fl_Button *button = new Fl_Button(60, 60, 50, 50, "label");
+  button->type(FL_NORMAL_BUTTON);
+
   window->end();
   window->show(argc, argv);
   return Fl::run();
